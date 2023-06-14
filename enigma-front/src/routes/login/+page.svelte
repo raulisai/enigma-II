6fe081e99941e5a0386af1fdaf41258199744a98
Davidcerv <davcertor@gmail.com>
2023-06-14 12:21:56 -0600

    implementacion de login

---
+++ enigma-front/src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<section>
+    <div class="container text-center">
+
+      <div class="row justify-content-center">
+        <div class="col ">
+            <h3>Login</h3>
+          <div class="row justify-content-center">
+            <div class="col-md-4">
+              <label for="inputEmail4" class="form-label">Email</label>
+              <input type="email" class="form-control" id="inputEmail4" />
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-md-4">
+              <label for="inputPassword4" class="form-label">Password</label>
+              <input type="password" class="form-control" id="inputPassword4" />
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-md-4 mt-5">
+                <button type="button" class="btn btn-outline-primary">Iniciar Session</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+</section>
